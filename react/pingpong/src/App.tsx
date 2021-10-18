7702a9bb8f797915d8170c9745fb57dd7a9fde3b
gohdong <aldehf420@naver.com>
2021-10-18 14:43:21 +0000

    pingpong

---
+++ react/pingpong/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  function apiCall(e:any){
+
+    // fetch("http://127.0.0.1:3000/api").then((res)=>console.log(res))
+    axios.get('http://192.168.0.45:3000/api').then((res)=>console.log(res))
+    
+  }
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <p
+          className="App-link"
+          // href="https://reactjs.org"
+          onClick={apiCall}
+          // target="_blank"
+          // rel="noopener noreferrer"
+        >
+          Learn React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
